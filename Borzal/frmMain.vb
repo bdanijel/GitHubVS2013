8ca1f29f6a90887ebd3811b05cbeb0e28f844743
bdanijel <lejinad5@gmail.com>
2015-04-27 18:01:48 +0200

    dodat je TabPage na frmUnos

--- Borzal/frmMain.vb
+++ Borzal/frmMain.vb
@@ -15,4 +15,8 @@
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         frmProizvod.Show()
     End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        frmUnos.Show()
+    End Sub
 End Class
---
+++ Borzal/frmUnos.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmUnos
+
+End Class
